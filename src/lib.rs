48c2d78afc51ecb1c2676fc031da37a78bfbfdf9
Leon Suchy <39390799+redpandamonium@users.noreply.github.com>
2022-02-20 18:41:42 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,7 @@ use bevy_reflect::TypeUuid;
 struct SomeReflectedType { }
 
 trait DerivedAsset: Asset_ { }
-pub trait AssetDynamic_: Send + Sync + 'static {}
-impl<T> AssetDynamic_ for T where T: Send + Sync + 'static {}
-pub trait Asset_: TypeUuid + AssetDynamic_ {}
-impl<T> Asset_ for T where T: TypeUuid + AssetDynamic_ {}
+pub trait Asset_: TypeUuid { }
+impl<T> Asset_ for T where T: TypeUuid { }
 
 impl DerivedAsset for SomeReflectedType { } //< the trait bound `SomeReflectedType: Asset` is not satisfied [E0277] the trait `Asset` is not implemented for `SomeReflectedType`
